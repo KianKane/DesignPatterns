ea8a2e06b28918300a87c6523424d7cc2e9e9a01
Kian Kane <hooded_kian@yahoo.co.uk>
2017-01-19 09:49:43 +0000

    Moved flying behaviour testing into its own method

--- src/strategypattern/Main.java
+++ src/strategypattern/Main.java
@@ -3,6 +3,10 @@ package strategypattern;
 public class Main
 {
     public static void main(String[] args){
+        testFlyingBehaviours();
+    }
+    
+    public static void testFlyingBehaviours(){
         Vehicle plane = new Plane();
         System.out.println("Plane: ");
         plane.fly();
